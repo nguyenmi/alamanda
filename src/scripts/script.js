999f4932f327c8467402c87547e2838b178e0da8
Mì Nguyễn <you@example.com>
2018-03-13 09:18:29 +0700

    initial code

---
+++ src/scripts/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//   $('.item-home').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+//     });
+// });
+
+$(document).ready(function(){
+  $('.item-home').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
